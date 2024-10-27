8435bbf6e3e740995e3aa5d2251bae98394dad8f
Madelsa <mahmoud-h-a@hotmail.com>
2024-10-27 23:27:16 +0400

    Completed the projects section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -14,6 +14,7 @@ const About = () => {
   return (
     <section className='c-space my-20' id="about">
         <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+            
             <div className='col-span-1 xl:row-span-3'>
                 <div className='grid-container'>
                     <img src="assets/portfPic.png" alt="grid-1" className='w-full sm:h-[276px] h-fit
@@ -21,12 +22,10 @@ const About = () => {
                      <div>
                         <p className='grid-headtext'>Hi, I'm Mahmoud</p>
                             <p className='grid-subtext'>
-                                I am a Computer Engineering undergraduate from Abu Dhabi University, 
-                                currently enrolled at 42 Abu Dhabi. I’m committed to becoming 
-                                a specialist in software development, leveraging skills in 
-                                Java, Python, C, C++, and more. With a focus on building efficient, 
-                                scalable solutions, I continuously strive to expand my expertise 
-                                in modern technologies and frameworks to solve complex challenges.
+                                A Computer Engineering undergraduate from Abu Dhabi University, 
+                                currently enrolled at 42 Abu Dhabi, with skills in Java, Python, C, C++, and more. 
+                                I’m committed to becoming a specialist in software development, 
+                                focusing on building efficient and scalable solutions.
                             </p>
                      </div>
                 </div>
@@ -50,8 +49,8 @@ const About = () => {
                     <div className='rounded-3xl w-full sm:h-[326px] 
                         h-fit flex justify-center items-center'>
                             <Globe 
-                                height={326}
-                                width={326}
+                                height={350}
+                                width={350}
                                 backgroundColor='rgba(0, 0, 0, 0)'
                                 backgroundImageOpacity={0.5}
                                 showAtmosphere
@@ -83,6 +82,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
+            
             <div className='xl:col-span-2 xl:row-span-3'>
                 <div className='grid-container'>
                     <img src="assets/grid3.png" alt="grid-3" className='w-full sm:h-[276px]
@@ -90,13 +90,14 @@ const About = () => {
                         <div>
                             <p className='grid-headtext'>My Passion for Coding</p>
                             <p className='grid-subtext'>
-                                I love solving problems and building things through code. 
-                                Programming isn't just my profession—it's my passion. 
-                                I enjoy exploring new technologies, and enhancing my skills.
+                                I really enjoy tackling problems and creating cool things with code. 
+                                For me, programming isn’t just a job; it’s a passion. 
+                                I love diving into new technologies and continuously leveling up my skills.
                             </p>
                         </div>
                 </div>
             </div>
+
             <div className='xl:col-span-1 xl:row-span-2'>
                 <div className='grid-container'>
                     <img src="assets/grid4.png" alt="grid-4" className='w-full md:h-[126px] sm:h-[276px] 
